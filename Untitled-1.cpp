ca84f52ebbb812eb0d7930f011e8826b1e59c0cf
Rohan Raj <rohan.raaj2@gmail.com>
2023-01-05 01:55:38 +0500

    Add files via upload

---
+++ Untitled-1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[9][9];
+    int     
+    int arr[9][9];
+    for (int i = 1; i <= 10; i ++)
+    {
+        for (int j = 1; j <= 10; j ++)
+        {
+            arr[i][j] = i * (j);
+            cout << arr[i][j] << " ";
+        }
+            cout << endl;
+            if (i == 10)
+            {
+                break;
+            }       
+            else
+            {
+                
+            }
+
+    }
+}
