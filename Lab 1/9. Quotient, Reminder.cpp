b58836dc40c98a4cb0c8f891fea8a82920cbe59f
Rohan Raj <rohan.raaj2@gmail.com>
2023-01-21 20:09:08 +0500

    Create 9. Quotient, Reminder.cpp

---
+++ Lab 1/9. Quotient, Reminder.cpp
@@ -0,0 +1,17 @@
+using namespace std;
+int main()
+{
+ /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+ int a;
+ int b;
+ int c;
+ int d;
+ cin >> a;
+ cin >> b;
+ c = a / b;
+ d = a - (b * c);
+
+ cout << c << " " << d;
+
+ return 0;
+}
