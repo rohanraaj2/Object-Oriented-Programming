35c95720f1e026d7c8f94b868ab385a69e997152
Rohan Raj <rohan.raaj2@gmail.com>
2023-05-31 18:15:15 +0500

    Update car_racing_game.py

--- Car Racing Game/car_racing_game.py
+++ Car Racing Game/car_racing_game.py
@@ -1,19 +1,18 @@
-class main:
+class Main:
     def __init__(self):
         for car_number in range(20):
             if car_number < 10:
-                car = DefaultCar().doAccelerate()
-                print ("Car", car_number, "has a current speed of", car.current_speed)
+                car = DefaultCar()
+                print("Car", car_number, "has a current speed of", car.current_speed)
             elif car_number >= 10:
-                car = RocketCar().boost()
+                car = RocketCar()
                 print("Car", car_number, "has a current speed of", car.current_speed)
 
 class Car:
     # A class representing a car object.
 
-    def __init__(self, name, current_speed=0, maximum_speed=0, acceleration=10.0):
-        # Initializes an instance of the Car class with the given name, current speed, maximum speed, and acceleration.
-        self.name = name
+    def __init__(self, current_speed=0, maximum_speed=0, acceleration=10.0):
+        # Initializes an instance of the Car class with the given current speed, maximum speed, and acceleration.
         self.current_speed = current_speed
         self.maximum_speed = maximum_speed
         self.acceleration = acceleration
@@ -22,9 +21,8 @@ class Car:
         # Returns the current speed of the car.
         return self.current_speed
 
-    def doAccelerate(self, acceleration):
+    def doAccelerate(self):
         # Accelerates the car by the given amount.
-        self.acceleration = acceleration
         self.current_speed += self.acceleration
         if self.current_speed > self.maximum_speed:
             self.current_speed = self.maximum_speed
@@ -32,19 +30,21 @@ class Car:
 class RocketCar(Car):
     # A class representing a rocket-powered car object.
 
-    def __init__(self, name, current_speed=0, maximum_speed=400, acceleration=10.0):
-        # Initializes an instance of the RocketCar class with the given name, current speed, maximum speed, and acceleration.
-        super().__init__(name, current_speed, maximum_speed, acceleration)
+    def __init__(self, current_speed=0, maximum_speed=400, acceleration=10.0):
+        # Initializes an instance of the RocketCar class with the given current speed, maximum speed, and acceleration.
+        super().__init__(current_speed, maximum_speed, acceleration)
 
     def boost(self):
         # Boosts the car by accelerating it by 20.0.
-        return super().doAccelerate(20.0)
+        super().doAccelerate(20.0)
+
 
 class DefaultCar(Car):
     # A class representing a default car object.
 
-    def __init__(self, name, current_speed=0, maximum_speed=200, acceleration=10.0):
-        # Initializes an instance of the DefaultCar class with the given name, current speed, maximum speed, and acceleration.
-        super().__init__(name, current_speed, maximum_speed, acceleration)
+    def __init__(self, current_speed=0, maximum_speed=200, acceleration=10.0):
+        # Initializes an instance of the DefaultCar class with the given current speed, maximum speed, and acceleration.
+        super().__init__(current_speed, maximum_speed, acceleration)
 
-instance = main()
+
+instance = Main()
